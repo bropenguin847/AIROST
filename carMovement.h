9a29a85c1bd75e8afffbe5c2bc1fa349eaf74c1e
bropenguin847 <106984547+bropenguin847@users.noreply.github.com>
2024-12-03 22:53:52 +0800

    updated motor speed

--- carMovement.h
+++ carMovement.h
@@ -6,7 +6,7 @@ int motorright1 = 25;
 int motorright2 = 33;
 int motorrightEn = 32;
 
-int motorspeed = 205;
+int motorspeed = 255;
 // If motor speed too fast, then IR sensor might have trouble doing line following
 // worth a discussion on motor speed. If speed too slow, the motor wont
 // start up. So far i found that speed 205 is okay
